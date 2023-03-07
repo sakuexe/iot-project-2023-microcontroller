16d66aab10ed86b12f5482bd48deb1e72f70ece2
Saku Karttunen <saku.karttunen@gmail.com>
2023-03-07 13:02:30 +0000

    added commnets, last formatting changes

--- main.cpp
+++ main.cpp
@@ -19,10 +19,19 @@
 
 #include <stdio.h>
 
+/* 
+    in the MLX90614, we changed the library's thermometer.read_temp() -function
+    into using a single char datatype variable as a parameter, instead of an int
+    this was done so that we could use 'A' for ambient and 'O' for object temp
+    ! So if the program doesn't work, check your library files or change the lines
+    of the program to use 0 for ambient temp and 1 for object temperature
+*/
 #include "MLX90614.h"
 // don't pay attention to the error
 #include <MQTTClientMbedOs.h>
 /*
+    Library credits
+    ---
     MLX90614 Library is based on Jens Strümper's library (minor changes)
     https://os.mbed.com/users/jensstruemper/code/MLX90614/
     MBED-MQTT library is the ARMmbed's own library (no changes)
@@ -31,8 +40,6 @@
 
 int main()
 {
-
-
     // MQTT Settings
 
     ESP8266Interface esp(MBED_CONF_APP_ESP_TX_PIN, MBED_CONF_APP_ESP_RX_PIN);
@@ -69,12 +76,13 @@ int main()
 
     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;       
     data.MQTTVersion = 3;
-    // char *id = MBED_CONF_APP_MQTT_ID;
-    // data.clientID.cstring = id;
     data.clientID.cstring = MBED_CONF_APP_MQTT_ID;
 
+    // initialize the buffer to be 64 bytes
+    // the length will have to get resized after assigning payload
     char buffer[64];
 
+    // initialize MQTT message
     MQTT::Message msg;
     msg.qos = MQTT::QOS0;
     msg.retained = false;
@@ -98,7 +106,11 @@ int main()
     
     printf("START\r\n");
     
-    for (int i = 0; i < 20; i++) {
+    // choose for how long one cycle of the program runs for
+    // there will be a delay of 2 seconds between each iteration
+    // we use a 5 minute cycle, this was counted with the function of
+    // (60 * desired_minutes) / 2
+    for (int i = 0; i < 150; i++) {
 
         // if green LED1 is on, that means temperature is being
         // read from the sensor
@@ -112,6 +124,8 @@ int main()
         // "target.printf_lib": "std"
         // this disables Mbed OS 6.0's 'minimal printf and snprintf'
         printf("IR Temperature: %.2f \r\n", objectTemp);
+        // the datasheet for MLx90614 recommends 20 ms delay between calculations
+        // Page 17/57 - 8.3.3.1. ERPROMwritesequence
         ThisThread::sleep_for(20ms);
 
         // read ambient temperature second
@@ -126,13 +140,14 @@ int main()
         sprintf(buffer, "{\"object\":%.2f,\"ambient\":%.2f}", objectTemp, ambientTemp);
         // update payload length to be the length of the message
         // instead of being consistantly 64 bytes
-        msg.payloadlen = strlen(buffer);    // <------ Muutettu
+        msg.payloadlen = strlen(buffer);
         client.publish(MBED_CONF_APP_MQTT_TOPIC, msg);
         // print that the topic was published
         printf("Published on topic: %s\n\r", MBED_CONF_APP_MQTT_TOPIC);
 
-        // wait before reading again
+        // wait 2 seconds before reading again
         ThisThread::sleep_for(2s);
     }
     printf("Loop is done, now stopping.\n\r");
+    return 0;
 }
